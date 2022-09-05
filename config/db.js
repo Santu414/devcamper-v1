605d2d1b53cd9fac8833435e04b08efec5c8603a
Dileep Reddy <dileepnagankera@gmail.com>
2022-09-05 20:27:49 +0530

    Creating Files Delete put post

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config({ path: "./config.env" });
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {});
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
