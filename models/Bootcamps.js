c810be940c38c9e486fc4b87796bdac4fd78f85e
Dileep Reddy <dileepnagankera@gmail.com>
2022-09-06 12:33:08 +0530

    Custom Error Handling  Mongoose Middleware

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BoootcampSchema = new mongoose.Schema({
   name: {
@@ -41,11 +43,9 @@ const BoootcampSchema = new mongoose.Schema({
     type: {
       type: String,
       enum: ["Point"],
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: "2dsphere",
     },
     formattedAddress: String,
@@ -103,4 +103,31 @@ const BoootcampSchema = new mongoose.Schema({
   },
 });
 
+//Create bootcamp slug from   the name
+BoootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocoder & create location field
+BoootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //Do Not save address in DB
+  this.address = undefined;
+
+  next();
+});
+
+module.exports = slugify;
 module.exports = mongoose.model("Bootcamp", BoootcampSchema);
